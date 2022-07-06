7fa55f633d29a81c8a7f4ca697a26afa6727bc4f
woowahan <woowahan@woowahan-MacBookPro-55.local>
2022-07-06 17:38:37 +0900

    메인 화면 기능 - 유효성 검사 및 버튼 활성

--- app/src/main/java/com/woowacamp/mail/MainActivity.kt
+++ app/src/main/java/com/woowacamp/mail/MainActivity.kt
@@ -1,12 +1,60 @@
 package com.woowacamp.mail
 
-import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.woowacamp.mail.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding get() = requireNotNull(_binding)
+
+    private var nicknameValid = false
+    private var emailValid = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            nicknameEdit.addTextChangedListener {
+                nicknameValid = if (it == null || it.length < 4) {
+                    nicknameLayout.error = getString(R.string.warn_nickname)
+                    false
+                } else {
+                    nicknameLayout.error = null
+                    true
+                }
+                nextButton.isEnabled = nicknameValid && emailValid
+            }
+            emailEdit.addTextChangedListener {
+                val regex = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+\$".toRegex()
+                emailValid = if (it == null || !it.matches(regex)) {
+                    emailLayout.error = getString(R.string.warn_email)
+                    false
+                } else {
+                    emailLayout.error = null
+                    true
+                }
+                nextButton.isEnabled = nicknameValid && emailValid
+            }
+
+            nextButton.setOnClickListener {
+                startEmailActivity()
+            }
+        }
+    }
+
+    private fun startEmailActivity() {
+//        startActivity(
+//            Intent()
+//        )
     }
 }
